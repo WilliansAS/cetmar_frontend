200f48b3be670b416689b155fac077f6a1de1ff0
WilliansAS <anthowill1608@gmail.com>
2025-10-26 18:09:51 -0500

    feat(school): Crear Dashboard de Control Escolar y enlazar en SchoolLayout

---
+++ src/modules/school/pages/DashboardSchool.vue
@@ -0,0 +1,14 @@
+<template>
+  <SchoolLayout>
+    <div class="flex justify-center pb-10">
+      <h1 class="font-bold text-4xl">¡Bienvenido de Nuevo, WilliansAS!</h1>
+    </div>
+
+    <StatsSchool />
+  </SchoolLayout>
+</template>
+
+<script setup lang="ts">
+import SchoolLayout from "../components/SchoolLayout.vue";
+import StatsSchool from "../components/StatsSchool.vue";
+</script>
